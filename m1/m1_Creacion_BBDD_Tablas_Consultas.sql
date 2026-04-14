47eae4c89c7886665ceb8cd6fd20a2121498bfec
José Páez <jose.paez@gmail.com>
2026-04-14 18:52:14 -0300

    Agregue un ejemplo

---
+++ m1/m1_Creacion_BBDD_Tablas_Consultas.sql
@@ -0,0 +1,4 @@
+/*
+  Script de creación y manipulación de la base de datos ComercioIT
+  Descripción: Este script crea la base de datos ComercioIT, sus tablas principales y realiza inserciones y consultas básicas.
+*/
